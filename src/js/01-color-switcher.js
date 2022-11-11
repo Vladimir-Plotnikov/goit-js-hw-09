ab6ba47d8ba771d6176230f86fa1d71ef355ae32
Volodymyr <vovdata@gmail.com>
2022-11-11 21:06:54 +0100

    HA 9
    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    startButton: document.querySelector('[data-start]'),
+    stopButton: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const changeColor = function () {
+    refs.body.style.backgroundColor = getRandomHexColor()
+}
+
+let timeFlow = null
+
+refs.startButton.addEventListener('click', function(){
+    timeFlow = setInterval(changeColor, 1000)
+    refs.startButton.disabled = true;
+}
+)  
+
+refs.stopButton.addEventListener('click', function() {
+    clearInterval(timeFlow);
+    refs.startButton.disabled = false;
+})
