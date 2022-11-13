04b7bd5bb953772f51ceedb0f9984ed630da73fb
Volodymyr <vovdata@gmail.com>
2022-11-13 22:12:32 +0100

    HA 9
    last

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,54 +10,67 @@ const refs = {
     secondsValue: document.querySelector('[data-seconds]'),
 }
 
-let timerId = null;
+let timer = null;
+
+console.log(refs.dataPicker);
+
+refs.startButton.disabled = true;
+
+//flatpickr
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
     onClose(selectedDates) {
-
-        // тут я обчислюю скільки залишилося до обраної дати
-        let ms = selectedDates[0] - new Date();
-        function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-        }
-
-        // тут я записую результат обчислення у табло
-       
-        function setDate() {
-            refs.secondsValue.innerHTML = convertMs(ms).seconds;
-            refs.minutesValue.innerHTML = convertMs(ms).minutes;
-            refs.hoursValue.innerHTML = convertMs(ms).hours;
-            refs.daysValue.innerHTML = convertMs(ms).days;
-        }
-        
-        // тут збирається все разом: закидаю ланцюжок від обчислення до запису в лічильник.
-        if (selectedDates[0] <= options.defaultDate) {
+        if (selectedDates[0] <= new Date()) {
             refs.startButton.disabled = true;
             window.alert("Please choose a date in the future")
         } else {
             refs.startButton.disabled = false;
-            refs.startButton.addEventListener('click', ()=>{
-                timerId = setInterval(setDate()),1000
-        })
+            refs.startButton.addEventListener('click', countDownTime);
+
+            function countDownTime() {
+                timer = setInterval(() => {
+                    refs.startButton.disabled = true;
+                    const dataChoosen = selectedDates[0].getTime()
+                    const nowTime = new Date().getTime();
+                    const timeLeft = dataChoosen - nowTime
+
+                    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+                    refs.daysValue.innerHTML = days < 10 ? addLeadingZero(days) : days;
+                    refs.hoursValue.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
+                    refs.minutesValue.innerHTML = minutes < 10 ? addLeadingZero(minutes) : minutes;
+                    refs.secondsValue.innerHTML = seconds < 10 ? addLeadingZero(seconds) : seconds;
+
+                    if (timeLeft < 1000) {
+                        clearInterval(timer);
+                        refs.startButton.disabled = false;
+                    }
+                }, 1000);
+            }
+            function addLeadingZero(value) {
+                const stringValue = String(value);
+                return stringValue.padStart(2, '0');
+            }
+            function convertMs(ms) {
+                const second = 1000;
+                const minute = second * 60;
+                const hour = minute * 60;
+                const day = hour * 24;
+
+                const days = Math.floor(ms / day);
+                const hours = Math.floor((ms % day) / hour);
+                const minutes = Math.floor(((ms % day) % hour) / minute);
+                const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+                return { days, hours, minutes, seconds };
+            }
+        }
+    },
             
-      }
-  },
 };
 
 flatpickr(refs.dataPicker, options)
-
-
