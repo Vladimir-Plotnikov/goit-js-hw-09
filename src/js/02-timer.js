31d16d0995f4c69c9ac08ffb12aa27fa32e898dd
Volodymyr <vovdata@gmail.com>
2022-11-13 16:03:29 +0100

    HA9
    4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,7 +9,6 @@ const refs = {
     minutesValue: document.querySelector('[data-minutes]'),
     secondsValue: document.querySelector('[data-seconds]'),
 }
-
 let timerId = null;
 
 const options = {
@@ -18,8 +17,10 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
+        let setDateSec = selectedDates[0].getTime();
+        let defDateSec = options.defaultDate.getTime();
       
-       let ms = selectedDates[0].getTime()
+       let ms = setDateSec-defDateSec
         function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -34,17 +35,22 @@ const options = {
   return { days, hours, minutes, seconds };
         }
         function setDate() {
-            refs.daysValue.innerHTML = convertMs(ms).days;
-            refs.hoursValue.innerHTML = convertMs(ms).hours;
-            refs.minutesValue.innerHTML = convertMs(ms).minutes;
             refs.secondsValue.innerHTML = convertMs(ms).seconds;
+            refs.minutesValue.innerHTML = convertMs(ms).minutes;
+            refs.hoursValue.innerHTML = convertMs(ms).hours;
+            refs.daysValue.innerHTML = convertMs(ms).days;
         }
         if (selectedDates[0] <= options.defaultDate) {
             refs.startButton.disabled = true;
             window.alert("Please choose a date in the future")
         } else {
             refs.startButton.disabled = false;
-            refs.startButton.addEventListener('click', setDate);
+            refs.startButton.addEventListener('click', countDown)
+            
+            function countDown() {
+                timerId = setInterval(setDate,1000)
+            }
+            console.log(timerId);
       }
   },
 };
