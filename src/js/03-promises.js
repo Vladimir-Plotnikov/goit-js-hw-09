a98519f98b379727e958a2edab86c3b7c15e23e7
Volodymyr <vovdata@gmail.com>
2022-11-14 14:09:31 +0100

    new commit
    new commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
